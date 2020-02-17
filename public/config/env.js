82d3ab148b9eb2fb039b675ac843e0349ac65601
root <root@vmi334380.contaboserver.net>
2020-02-17 12:26:38 +0100

    test

---
+++ public/config/env.js
@@ -0,0 +1,17 @@
+window.env = {
+    api: {
+        authUrl: 'http://localhost:9002/api/v2/barong',
+        tradeUrl: 'http://localhost:9002/api/v2/peatio',
+        applogicUrl: 'http://localhost:9002/api/v2/applogic',
+        rangerUrl: '',
+    },
+    minutesUntilAutoLogout: '5',
+    withCredentials: false,
+    gaTrackerKey: '',
+    rangerReconnectPeriod: '1',
+    msAlertDisplayTime: '5000',
+    incrementalOrderBook: true,
+    isResizable: false,
+    isDraggable: false,
+    languages: ['en', 'ru'],
+};
